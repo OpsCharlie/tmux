819f40dd654bf2ebe2268b4bec903108f5173a66
Charlietje <verstraete.carl@gmail.com>
2017-07-14 16:02:55 +0200

    bug in if statemant

--- tmux/updates_available.sh
+++ tmux/updates_available.sh
@@ -4,7 +4,7 @@ TMP=/tmp/updates.tmux
 
 [ -e $TMP ] ||  /usr/lib/update-notifier/apt-check &>$TMP
 
-if test $(find $TMP -mmin +10 &>/dev/null); then
+if test $(find $TMP -mmin +10); then
     /usr/lib/update-notifier/apt-check &>$TMP
 fi
 
@@ -16,7 +16,7 @@ if [ $UPDATES -ne 0 ]; then
 fi
 
 if [ $SECUPDS -ne 0 ]; then
-   printf "#[default]#[fg=red]"; printf "%d!! " $SECUPDS; printf "#[default]#[fg=colour136]"; 
+   printf "#[default]#[fg=red]"; printf "%d!! " $SECUPDS; printf "#[default]#[fg=colour136]";
 fi
 
 if [[ $UPDATES -ne 0 ]] || [[ $SECUPDS -ne 0 ]]; then
